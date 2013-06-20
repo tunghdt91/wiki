438e6b3a1ea6528a0fbeeef8ab84085a51fc8aff
Vu Dang Tung <tunghdt91@gmail.com>
2013-06-20 09:30:19 +0700

    create project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Wiki::Application.config.secret_token = '5a6ec2edd2df4dd6317e784e4fda32f940f15890d9fb9d3eb8c22ba27acf60db5790ac9899003f248434b9722a72bfe175228da3ce312301e50c459b9c3b9c59'
